f162fa89a92b15c17b2a4d05a7c8bfceab3db0fb
sssunda <sundaeun93@gmail.com>
2019-09-26 16:44:06 +0900

    implement django rest server

--- rest_server/server/models.py
+++ rest_server/server/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class DailyCpu(models.Model):
+    no = models.AutoField(primary_key=True)
+    server_name = models.CharField(max_length=30, blank=True, null=True)
+    server_ip = models.CharField(max_length=15, blank=True, null=True)
+    core_cnt = models.IntegerField(blank=True, null=True)
+    core_percent = models.FloatField(blank=True, null=True)
+    load_avg_1min = models.FloatField(blank=True, null=True)
+    load_avg_5min = models.FloatField(blank=True, null=True)
+    load_avg_15min = models.FloatField(blank=True, null=True)
+    stored_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'daily_cpu'
+
+
+class DailyMem(models.Model):
+    no = models.AutoField(primary_key=True)
+    server_name = models.CharField(max_length=30, blank=True, null=True)
+    server_ip = models.CharField(max_length=15, blank=True, null=True)
+    total = models.BigIntegerField(blank=True, null=True)
+    available = models.BigIntegerField(blank=True, null=True)
+    used = models.BigIntegerField(blank=True, null=True)
+    free = models.BigIntegerField(blank=True, null=True)
+    buffers_cached = models.BigIntegerField(blank=True, null=True)
+    stored_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'daily_mem'
+
+class ServerList(models.Model):
+    no = models.AutoField(primary_key=True)
+    server_name = models.CharField(max_length=30, blank=True, null=True)
+    server_ip = models.CharField(max_length=15, blank=True, null=True)
+    stored_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'server_list'
--- rest_server/server/views.py
+++ rest_server/server/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from .models import DailyCpu, DailyMem, ServerList
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from datetime import datetime
 
-# Create your views here.
+@api_view(['GET'])
+def view(request):
+    server_list = ServerList.objects.all().values_list('server_name', flat=True)
+    data = []
+
+    for server in server_list:
+        send_data = {}
+        mem_no = DailyMem.objects.filter(server_name = server).order_by('-no').values_list('no',flat=True)[:288]
+        cpu_no = DailyCpu.objects.filter(server_name = server).order_by('-no').values_list('no',flat=True)[:288]
+
+        mem_query = DailyMem.objects.filter(no__in = list(mem_no)).order_by('no')
+        cpu_query = DailyCpu.objects.filter(no__in = list(cpu_no)).order_by('no')
+
+        timeseries = list(map(lambda x : x.strftime('%Y-%m-%d %H:%M:%S'), mem_query.values_list('stored_time', flat=True)))
+        total_memory = list(mem_query.values_list('total', flat=True))
+        available_memory = list(mem_query.values_list('available', flat=True))    
+        used_memory = list(mem_query.values_list('used', flat=True))
+        free_memory = list(mem_query.values_list('free', flat=True))
+        buffers_cached_memory = list(mem_query.values_list('buffers_cached', flat=True))
+        cpu_core_cnt = list(cpu_query.values_list('core_cnt', flat=True))
+        cpu_core_percent = list(cpu_query.values_list('core_percent', flat=True))
+        cpu_load_avg_1min = list(cpu_query.values_list('load_avg_1min', flat=True))
+        cpu_load_avg_5min = list(cpu_query.values_list('load_avg_5min', flat=True))
+        cpu_load_avg_15min = list(cpu_query.values_list('load_avg_15min', flat=True))
+
+        send_data['server_name'] = server 
+        send_data['data'] = {
+            'timeseries' : timeseries,
+            'total_memory' : total_memory,
+            'available_memory' : available_memory,
+            'used_memory' : used_memory,
+            'free_memory' : free_memory,
+            'buffers_cached_memory' : buffers_cached_memory,
+            'cpu_core_cnt' : cpu_core_cnt,
+            'cpu_core_percent' : cpu_core_percent,
+            'cpu_load_avg_1min' : cpu_load_avg_1min,
+            'cpu_load_avg_5min' : cpu_load_avg_5min,
+            'cpu_load_avg_15min' : cpu_load_avg_15min,
+            }
+
+        data.append(send_data)
+
+    return Response({'total': len(server_list), 'data': data})
