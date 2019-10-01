31cae39715d7df47694b48060256c832eeabf3fb
sssunda <sundaeun93@gmail.com>
2019-10-01 20:56:27 +0900

    modify urls order

--- rest_server/rest_server/urls.py
+++ rest_server/rest_server/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('api/memory', views.view_mem),
     path('api/memory/<server_name>', views.view_mem),
     path('api/cpu', views.view_cpu),
-    path('api/cpu/<server_name>', views.view_cpu),
     path('api/cpu/server_list', views.view_serverlist),
+    path('api/cpu/<server_name>', views.view_cpu),
     path('admin/', admin.site.urls),
 ]
